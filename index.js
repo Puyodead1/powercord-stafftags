41f8766f657e422be98d003275e39c598dea58d6
Puyodead1 <23562356riley@gmail.com>
2020-09-20 23:10:15 -0400

    ahhhhhhhhhh

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /* Essential Packages */
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
-const { getModule } = require('powercord/webpack');
+const { React, getModule } = require('powercord/webpack');
 
 /* Plugin Specific Packages */
-const { ContextMenu } = require('powercord/components');
-const { getOwnerInstance } = require('powercord/util');
 const { name, shorthand } = require('./manifest.json'); // -> name: 'Project Name', shorthand: 'pName'
 
 /* Settings */
 const Settings = require('./Components/Settings.jsx');
+const Tag = require('./Components/Tag');
 
 module.exports = class MyPlugin extends Plugin {
     /* Entry Point */
     async startPlugin() {
+        this.loadStylesheet('style.scss');
         /* Register Settings */
         powercord.api.settings.registerSettings(shorthand, {
             category: this.entityID,
@@ -21,16 +21,15 @@ module.exports = class MyPlugin extends Plugin {
             render: Settings // The React component to render. In this case, the imported Settings file
         });
 
+        this.classes = await getModule(['profileBadgeStaff']);
+
         /**
          * The following is an example of adding an option to an image's context menu,
          * which changes the image to a picture of a dog/cat based on the setting.
          */
 
         // Discord is made up of thousands of modules, the following lines look for the modules which meet the specified filters.
-        this.imageWrapper = await getModule(['imageWrapper']);
-        const injectInto = await getModule(
-            m => m.default && m.default.displayName === 'MessageContextMenu'
-        );
+        this.MessageTimestamp = await getModule(['MessageTimestamp']);
 
         /**
          * The following injects a function into the specified module.
@@ -39,31 +38,16 @@ module.exports = class MyPlugin extends Plugin {
          * 3: The function name you want to target.
          * 4: The function you want to inject.
          */
-        inject(shorthand, injectInto, 'default', (event, res) => {
-            const target = event[0].target;
+        inject(shorthand, this.MessageTimestamp, 'default', (args, res) => {
+            const id = args[0].message.author.id;
+            console.log(id);
+            const header = res.props.children[1];
 
-            /** Only add to the Menu when the target is an image
-             * and the parent element(module) contains an imageWrapper */
-            if (target.tagName.toLowerCase() === 'img') {
-                let displayCat = this.settings.get('displayCat');
+            const element = React.createElement(Tag, { userid: id });
 
-                // Push new ContextMenu item to res.props.children
-                res.props.children.push(
-                    ...ContextMenu.renderRawItems([
-                        {
-                            type: 'button',
-                            name: `Display ${displayCat ? 'Cat' : 'Dog'}`,
-                            id: `${shorthand}-button` /* When adding items, make sure all of the id's are different */,
-                            onClick: () => {
-                                /* This is the function that is executed when the button is clicked */
-                                target.src = displayCat
-                                    ? 'https://i.imgur.com/cqAklTF_d.webp'
-                                    : 'https://i.imgur.com/rpQdRoY_d.webp?maxwidth=728&fidelity=grand';
-                            }
-                        }
-                    ])
-                );
-            }
+            const size = header.props.children.length;
+            header.props.children[size] = header.props.children[size - 1];
+            header.props.children[size - 1] = element;
 
             return res;
         });
